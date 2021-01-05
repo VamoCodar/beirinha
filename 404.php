63ebc51a7b270c080e049d2a664171d22155dd74
pedropimenta <znzdesign@gmail.com>
2021-01-05 10:01:41 +0000

    pagina 404

--- 404.php
+++ 404.php
@@ -1,7 +1,33 @@
-<?php get_header('404'); ?>
+<?php get_header(); ?>
 
+<style>
+	/* 404 */
+.error404 .mt-5 .row{
+	display: block;
+}
+#notfound {  position: relative;  height: 60vh;}
+#notfound .notfound {  position: absolute;  left: 50%;  top: 50%;  -webkit-transform: translate(-50%, -50%);      -ms-transform: translate(-50%, -50%);          transform: translate(-50%, -50%);}
+.notfound {  max-width: 520px;  width: 100%;  line-height: 1.4;  text-align: center;}
+.notfound .notfound-404 {  position: relative;  height: 200px;  margin: 0px auto 20px;  z-index: -1;}
+ 
+.notfound .notfound-404 h1 {  font-size: 236px;  font-weight: 200;  margin: 0px;  color: #f1f1f1;  text-transform: uppercase;  position: absolute;
+  left: 50%;  top: 50%;  -webkit-transform: translate(-50%, -50%);      -ms-transform: translate(-50%, -50%);          transform: translate(-50%, -50%);}
+ 
+.notfound .notfound-404 h2 {  font-size: 28px;  font-weight: 400;  text-transform: uppercase;  color: #f1f1f1;  background: gray;  padding: 10px 5px;
+  margin: auto;  display: inline-block;  position: absolute;  bottom: 0px;  left: 0;  right: 0;}
+ 
+.notfound a {  display: inline-block;  font-weight: 700;  text-decoration: none;  color: #fff;  text-transform: uppercase;  padding: 13px 23px;
+  background: #EA882D;  font-size: 18px;  -webkit-transition: 0.2s all;  transition: 0.2s all;}
+ 
+.notfound a:hover {  color: #000;  background: #EA882D;}
 
+@media only screen and (max-width: 480px) {
+  .notfound .notfound-404 {    height: 148px;    margin: 0px auto 10px;  }
+  .notfound .notfound-404 h1 {    font-size: 86px;  }
+  .notfound .notfound-404 h2 {    font-size: 16px;  }
+}
 
+</style>
 
 
 	<div id="notfound">
